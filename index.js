e16a78e4912c470e78f1e55ebe647082fdb245f5
Jason Wiram <jwiram@gmail.com>
2019-06-07 20:38:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -162,8 +162,9 @@ function moveDodgerRight() {
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
    const left = positionToInteger(DODGER.style.left)
-   if (left > 360)
-   DODGER.style.left = `${left + 4}px`
+   if (left < 360) {
+     DODGER.style.left = `${left + 4}px`
+   }
 }
 
 /**
