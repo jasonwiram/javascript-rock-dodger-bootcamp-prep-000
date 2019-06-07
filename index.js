5cdccef678bb9b319e87d826c282dc3a41da0a58
Jason Wiram <jwiram@gmail.com>
2019-06-07 20:32:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -42,69 +42,44 @@ function checkCollision(rock) {
   }
 }
 
-function createRock(x) {
+function createRock(x)
+{
   const rock = document.createElement('div')
-
   rock.className = 'rock'
-  rock.style.left = `${x}px`
-
-  // Hmmm, why would we have used `var` here?
+  rock.style.left = `${x}px`;
   var top = 0
-
-  rock.style.top = top
+  top = rock.style.top;
 
   /**
    * Now that we have a rock, we'll need to append
    * it to GAME and move it downwards.
    */
-
-  GAME.appendChild(rock);
+   //appends rock to game.
+   GAME.appendChild(rock)
 
   /**
    * This function moves the rock. (2 pixels at a time
    * seems like a good pace.)
    */
-  function moveRock() {
-    // implement me!
-    // (use the comments below to guide you!)
-    /**
-     * If a rock collides with the DODGER,
-     * we should call endGame()
-     */
-     
-     if (checkCollision(rock)) {
-       endGame();
+function moveRock()
+  {
+     rock.style.top = `${top += 2}px`;
+     //if a rock collides with the dodger, call end game function.
+     if(checkCollision(rock)===true)
+     {
+       return endGame();
      }
-
-    /**
-     * Otherwise, if the rock hasn't reached the bottom of
-     * the GAME, we want to move it again.
-     */
-     
-     if (top < GAME_HEIGHT) {
+     //if the rock is not at the bottom, move the rock.
+     if(top < GAME_HEIGHT)
+     {
        window.requestAnimationFrame(moveRock);
      }
-
-    /**
-     * But if the rock *has* reached the bottom of the GAME,
-     * we should remove the rock from the DOM
-     */
-     
-     else {
+     //if rock is at the bottom of the game, remove the rock.
+     else
+     {
        rock.remove();
      }
   }
-
-  // We should kick of the animation of the rock around here
-  window.requestAnimationFrame(moveRock);
-
-  // Add the rock to ROCKS so that we can remove all rocks
-  // when there's a collision
-  ROCKS.push(rock)
-
-  // Finally, return the rock element you've created
-  return rock
-}
 
 /**
  * End the game by clearing `gameInterval`,
