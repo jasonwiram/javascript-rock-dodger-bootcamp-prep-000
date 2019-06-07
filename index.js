9a857b723a830e31385a486aac40a4cc4fd66316
Jason Wiram <jwiram@gmail.com>
2019-06-07 20:28:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -130,9 +130,14 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
-   if(e.which === LEFT_ARROW) {
+   if (e.which === LEFT_ARROW) {
+     e.stopPropagation()
+     e.preventDefault();
      moveDodgerLeft();
-   } else if(e.which === RIGHT_ARROW) {
+   } 
+   else if (e.which === RIGHT_ARROW) {
+     e.stopPropagation()
+     e.preventDefault();
      moveDodgerRight();
    }
 }
